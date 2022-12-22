41015754ed88582452298a421f5a4be5ce1a31e8
rkram0901 <104410052+rkram0901@users.noreply.github.com>
2022-12-22 21:14:11 +0530

    Update backendUrl.js

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,4 @@
-const backendUrl = "http://localhost:4000";
+const backendUrl = "https://66h4wes6xoyrfq4vcuhmniddxi0voter.lambda-url.us-east-1.on.aws";
 // const backendUrl = "https://stackuflow.herokuapp.com";
 
 export default backendUrl;
