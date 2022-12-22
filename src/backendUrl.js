f4d89bb5d7df2619088f454dc8807e6f912e1dc2
rkram0901 <104410052+rkram0901@users.noreply.github.com>
2022-12-23 01:01:07 +0530

    Update backendUrl.js

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://4vkjk0kuob.execute-api.us-east-1.amazonaws.com/dev";
+const backendUrl = "https://vhdicv3w9b.execute-api.us-east-1.amazonaws.com/qa";
 // const backendUrl = "https://stackuflow.herokuapp.com";
 
 export default backendUrl;
