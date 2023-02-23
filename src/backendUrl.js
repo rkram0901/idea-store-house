6a15e3e3316cd77a95635374ffd37df061833b9e
rkram0901 <104410052+rkram0901@users.noreply.github.com>
2023-02-23 19:34:37 +0530

    Update backendUrl.js

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://vhdicv3w9b.execute-api.us-east-1.amazonaws.com/qa";
+//const backendUrl = "https://vhdicv3w9b.execute-api.us-east-1.amazonaws.com/qa";
 // const backendUrl = "https://stackuflow.herokuapp.com";
-
+const backendUrl = "https://ispaceserver.azurewebsites.net";
 export default backendUrl;
